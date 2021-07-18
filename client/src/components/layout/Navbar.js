7f4eff0dfab7fe583903067d6741ca6599c16b0a
Daniel Madhala <danielmad87@gmail.com>
2021-07-18 18:00:02 +0300

    Initial React Setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <l1>
+          <Link to="/">Home</Link>
+        </l1>
+        <l1>
+          <Link to="/about">About</Link>
+        </l1>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+export default Navbar;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
 const port = process.env.PORT || 5000;
+//Connect Database
+connectDB();
+
+// Except body data
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the ContactKeeper API' });
@@ -12,5 +18,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 app.listen(port, () => {
-  console.log(`Server lisenong on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
